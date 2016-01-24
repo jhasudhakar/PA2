f8cd9a80d08149d837c3e7628d74ff5fc00cc973
Nick N Guo <nick.n.guo@gmail.com>
2016-01-23 21:45:36 -0800

    updated graph_gen to ensure generated graphs are connected

--- graph_gen.py
+++ graph_gen.py
@@ -14,10 +14,10 @@ from networkx import *
 #   p:   Probability that an edge exists between vertex u and v.
 #        p must be in (0.0, 1.0].
 #   w:   Max edge weight possible.
-#        Edge weights will be randomly selected from [0.0, w].
+#        Edge weights will be randomly selected from [1.0, w].
 
 n = 100
-p = 0.3
+p = 0.5
 w = 10
 
 # DO NOT EDIT BELOW, REPEAT: DO NOT EDIT BELOW.
@@ -25,6 +25,11 @@ w = 10
 print '\nGenerating graph with n={0}, p={1}, and w={2}\n'.format(n, p, w)
 
 g = fast_gnp_random_graph(n, p)
+
+vertices = range(n)
+random.shuffle(vertices)
+for i in range(n):
+    g.add_edge(vertices[i - 1], vertices[i])
 
 print 'Graph generation complete\n'
 
@@ -37,7 +42,7 @@ print 'Writing out to file: {0}\n'.format(filename)
 output_file.write('0\n')
 
 for e in g.edges():
-    output_file.write('{0} {1} {2:.4f}\n'.format(e[0], e[1], w*random.random()))
+    output_file.write('{0} {1} {2:.4f}\n'.format(e[0], e[1], 1 + (w-1)*random.random()))
 
 output_file.close()
 
